82779ddb0af487754b7a9056972e5698cd06e683
El-Nico <nicholasc1665@yahoo.com>
2020-04-11 13:08:48 +0100

    initialized and created files

---
+++ src/cafejava/CafeRunner.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cafejava;
+
+/**
+ *
+ * @author Nicholas
+ */
+public class CafeRunner {
+    public static void runCafe(){
+        
+    }
+}
